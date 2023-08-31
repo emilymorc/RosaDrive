2a3c4b6432bfef3870415e68a570268295bbb7d3
Emily <emily.moreno@uptc.edu.co>
2023-08-30 21:36:56 -0500

    Se crea la conexion entre las curl y crear usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Login2Component} from "./login2/login2.component";
-import {CreateAccountComponent} from "./menu/create-account/create-account.component";
+import {CreateAccountComponent} from "./create-account/create-account.component";
 import {RouterModule, Routes} from "@angular/router";
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LogginComponent } from './loggin/loggin.component';
-import { CreateAccountComponent } from './menu/create-account/create-account.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 import { Login2Component } from './login2/login2.component';
 import {RouterOutlet} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
