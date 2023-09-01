8f662c1969891357d19812dbf78a0a0aead1abfa
Jorge Ivan <joivmaji@gmail.com>
2023-09-01 16:50:23 -0500

    Se realizo refacorizacion de carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,6 @@ import { AppComponent } from './app.component';
 import {SidenavComponent} from "./sidenav/sidenav.component";
 import {BodyComponent} from "./body/body.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
-import {ProductsComponent} from "./products/products.component";
-import {StatisticsComponent} from "./statistics/statistics.component";
-import {CoupensComponent} from "./coupens/coupens.component";
-import {PagesComponent} from "./pages/pages.component";
-import {MediaComponent} from "./media/media.component";
-import {SettingsComponent} from "./settings/settings.component";
 import {AppRoutingModule} from "./app-routing.module";
 import {SublevelMenuComponent} from "./sidenav/sublevel-menu.components";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
@@ -37,12 +31,6 @@ import { LandingComponent } from './landing/landing.component';
     BodyComponent,
     SidenavComponent,
     DashboardComponent,
-    ProductsComponent,
-    StatisticsComponent,
-    CoupensComponent,
-    PagesComponent,
-    MediaComponent,
-    SettingsComponent,
     SublevelMenuComponent,
     CreateAccountComponent,
     Login2Component,
