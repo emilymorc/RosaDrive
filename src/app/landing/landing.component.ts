baf66f4f559de3a16748eb4e32c021ef1d18ef8b
nicolasllanos21 <nicolas.llanos@uptc.edu.co>
2023-09-07 15:39:45 -0500

    Upgrade landingPage

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -6,24 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
-  services: any[] = [
-    {
-      title: 'Latoneria',
-      description: 'El proceso consiste en restaurar las piezas del vehículo que han sido golpeadas.\n' +
-        'Para esto es importante implementar mecanismos para corregir las ondulaciones presentes\n' +
-        'en una lamina o chapa y hacer que estas tomen la forma original del fabricante.'
-    },
-    {
-      title: 'Pintura',
-      description: 'El proceso consiste en restaurar las piezas del vehículo que han sido golpeadas.\n' +
-        'Para esto es importante implementar mecanismos para corregir las ondulaciones presentes\n' +
-        'en una lamina o chapa y hacer que estas tomen la forma original del fabricante'
-    },
-    {
-      title: 'Mecanica',
-      description: 'El proceso consiste en restaurar las piezas del vehículo que han sido golpeadas.\n' +
-        'Para esto es importante implementar mecanismos para corregir las ondulaciones presentes\n' +
-        'en una lamina o chapa y hacer que estas tomen la forma original del fabricante'
-    }
-  ];
+
 }
