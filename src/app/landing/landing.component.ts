7b596f9c2791d7a5a9681dbecaae96282daf3124
nicolasllanos21 <nicolas.llanos@uptc.edu.co>
2023-09-08 10:40:35 -0500

    Se agregaron beneficios en landingpage

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
-
+  services: any[] = [
+    {
+      title: 'ISO 9001',
+      description: 'Nuestros procesos son certificados.'
+    },
+    {
+      title: 'Cabina de pintura',
+      description: 'Aplicación y secado en condiciones optimas'
+    },
+    {
+      title: 'Laboratorio propio',
+      description: 'Mezcla computarizada de pinturas de la mejor calidad'
+    },
+    {
+      title: 'Instalaciones',
+      description: 'Mas de 10.000 M² distribuidos técnicamente'
+    },
+    {
+      title: 'Factor Humano',
+      description: 'Contamos con\n' +
+        'técnicos en todas\n' +
+        'nuestras áreas'
+    },
+    {
+      title: 'Bancada estiraje',
+      description: 'Alineación de\n' +
+        'carrocería\n' +
+        'certificada'
+    },
+    {
+      title: 'Diagnostico',
+      description: 'Equipos electrónicos\n' +
+        'diagnostican con\n' +
+        'precisión\n' +
+        '\n '
+    },
+    {
+      title: 'Soldadura de punto',
+      description: 'Equipos garantizan\n' +
+        'la originalidad\n' +
+        'del vehículo'
+    },
+    {
+      title: 'Calidad certificada',
+      description: 'Todos los procesos\n' +
+        'cumplen estándares\n' +
+        'de calidad'
+    }
+  ];
 }
