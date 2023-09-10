3ef320b80b71c861a60880775b1edc271bfadbab
Emily <emily.moreno@uptc.edu.co>
2023-09-09 21:14:53 -0500

    Se crea el componente para las historias de los automotores

---
+++ src/app/autostorie/autostorie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autostorie',
+  templateUrl: './autostorie.component.html',
+  styleUrls: ['./autostorie.component.css']
+})
+export class AutostorieComponent {
+
+}
