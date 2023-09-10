69af709959f2b0da316904980e11f99866ac3f57
nicolasllanos21 <nicolas.llanos@uptc.edu.co>
2023-09-10 11:02:54 -0500

    Se agrega validacion de campos en el login

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -19,6 +19,14 @@ export class Login2Component implements OnInit {
   }
 
   onSubmit() {
+    if (this.email.trim() === '' || this.password.trim() === '') {
+      this.toastr.error("Por favor, complete todos los campos", "Campos Vacios");
+      return;
+    }
+    if (!this.validateEmail(this.email)) {
+      this.toastr.error("Por favor, ingrese un correo valido", "Correo Invalido");
+      return;
+    }
     this.authService.logear(this.email, this.password)
       .subscribe((response:any) => {
         const token = response.data.TOKEN;
@@ -34,6 +42,10 @@ export class Login2Component implements OnInit {
     );
   }
 
+  validateEmail(email: string): boolean {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(email);
+  }
   logout(){
     this.authService.logout();
   }
