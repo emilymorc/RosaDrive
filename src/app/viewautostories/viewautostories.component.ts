224784b78a97bfd2094efd2c002d6c7857052aba
Emily <emily.moreno@uptc.edu.co>
2023-09-10 22:02:47 -0500

    Se incorpora el componente de ver historiales

---
+++ src/app/viewautostories/viewautostories.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewautostories',
+  templateUrl: './viewautostories.component.html',
+  styleUrls: ['./viewautostories.component.css']
+})
+export class ViewautostoriesComponent {
+
+}
