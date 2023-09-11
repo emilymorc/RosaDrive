bdad9d4f79a975f2c3a42f21752ba023b9b76905
Jorge Ivan <joivmaji@gmail.com>
2023-09-10 22:00:05 -0500

    Filtro por apellido en lista de usuarios y modificaión de los botones de paginación

--- src/app/view-users/view-users.component.ts
+++ src/app/view-users/view-users.component.ts
@@ -12,71 +12,21 @@ export class ViewUsersComponent implements OnInit{
     maxSize: number = 5;
     orderBy: string | null = null;
     isAsc: boolean = true;
+    filtroApellido: string = '';
 
-  users: any[] = [
-      {
-          "ID_USER": 1,
-          "FIRST_NAME": "John",
-          "LAST_NAME": "Doe",
-          "PASSWORD": "$2a$10$VxNTUNM9Hq9qIztn3fZTGOPmXZKL8XwIGarxHUcAl9PfeKm0iOIpu",
-          "REGISTRATION_DATE": "2023-09-09T13:40:53.000Z",
-          "IDENTIFICATION_TYPE": "cc",
-          "IDENTIFICATION_NUMBER": "123456789",
-          "EMAIL": "johndoe@example.com"
-      },
-      {
-          "ID_USER": 2,
-          "FIRST_NAME": "Jane",
-          "LAST_NAME": "Smith",
-          "PASSWORD": "$2a$10$btDwo3ievjsCnJeGvlGVl.2y8FSH3WDKoiY5CyhHnntYz7dn9pQxG",
-          "REGISTRATION_DATE": "2023-09-10T18:21:54.000Z",
-          "IDENTIFICATION_TYPE": null,
-          "IDENTIFICATION_NUMBER": null,
-          "EMAIL": "janesmith@hotmail.com"
-      },
-      {
-          "ID_USER": 3,
-          "FIRST_NAME": "Michael",
-          "LAST_NAME": "Johnson",
-          "PASSWORD": "$2a$10$iIBGn7HZ2bG6A3BULsPKh.1LkOuVwW3IMp4muScmN82Zp5DKLZ4ou",
-          "REGISTRATION_DATE": "2023-09-10T18:35:49.000Z",
-          "IDENTIFICATION_TYPE": null,
-          "IDENTIFICATION_NUMBER": null,
-          "EMAIL": "michael.johnson@gmail.com"
-      },
-      {
-          "ID_USER": 4,
-          "FIRST_NAME": "Emily",
-          "LAST_NAME": "Brown",
-          "PASSWORD": "$2a$10$M8y9tlwmZbWGnUlTS62MHuT2DQoBXVOJoWu66YAUkCuJ4bZu13dS.",
-          "REGISTRATION_DATE": "2023-09-10T20:05:00.000Z",
-          "IDENTIFICATION_TYPE": null,
-          "IDENTIFICATION_NUMBER": null,
-          "EMAIL": "emily.brown@hotmail.com"
-      },
-      {
-          "ID_USER": 5,
-          "FIRST_NAME": "David",
-          "LAST_NAME": "Davis",
-          "PASSWORD": "$2a$10$DNDM8zt6Vbq3P7sBCY/bzu6ib9XyxB2DeL4ybd.QJ9RAKWjICxOHu",
-          "REGISTRATION_DATE": "2023-09-10T20:17:43.000Z",
-          "IDENTIFICATION_TYPE": null,
-          "IDENTIFICATION_NUMBER": null,
-          "EMAIL": "david.davis@gmail.com"
-      }
-  ];
+  users: any[] = [];
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    /*this.userService.getUsers().subscribe(
+    this.userService.getUsers().subscribe(
         data => {
           this.users = data;
         },
         error => {
           console.error('Error al obtener usuarios:', error);
         }
-    );*/
+    );
   }
     onPageChange(event: any): void {
         this.currentPage = event.page;
@@ -109,6 +59,13 @@ export class ViewUsersComponent implements OnInit{
         });
     }
 
-
-
+    filtrarPorApellido(): any[] {
+        if (this.filtroApellido) {
+            return this.users.filter(dato =>
+                dato.LAST_NAME && dato.LAST_NAME.toLowerCase().includes(this.filtroApellido.toLowerCase())
+            );
+        } else {
+            return this.users;
+        }
+    }
 }
