54efd76b91220d38efb008b13ba1f6370ae75d88
Jorge Ivan <joivmaji@gmail.com>
2023-09-10 23:06:24 -0500

    Arreglo en la validación del token de servicio de ususarios

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -7,14 +7,16 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
     private apiUrl = 'https://app-e988bfc5-a6ee-41bb-a6af-e418a4b27735.cleverapps.io/api/users/getUsers';
-    private accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiaWF0IjoxNjk0MzgxNjEwLCJleHAiOjE2OTQ0NjgwMTB9.avJWxnQ-Nom5ICE4tMB1vEWptsDf_MP5SI1qjzP_xuM';
+    private valor: string | null = localStorage.getItem('token')
+    private valorCasteado: string | number | (string | number)[] = this.valor as string | number | (string | number)[];
+
 
     constructor(private http: HttpClient) { }
 
     getUsers(): Observable<any> {
         const headers = new HttpHeaders({
             'User-Agent': 'Insomnia/2023.5.5',
-            'x-access-token': this.accessToken
+            'x-access-token': this.valorCasteado
         });
 
         return this.http.get(this.apiUrl, { headers });
