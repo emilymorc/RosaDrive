86802be17286c85e48ca121ce4ab5bb8f31ee92f
Ivan Martinez <125705317+IvanM2002@users.noreply.github.com>
2023-09-29 11:18:19 -0500

    arreglo en servicio de historiales y modificacion tabla de ver ordenes de servicio

--- src/app/servicios/history.service.ts
+++ src/app/servicios/history.service.ts
@@ -6,19 +6,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class HistoryService {
-  private apiUrl = 'https://app-e988bfc5-a6ee-41bb-a6af-e418a4b27735.cleverapps.io/api/stories/getStories';
-  private accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiaWF0IjoxNjk0MzgxNjEwLCJleHAiOjE2OTQ0NjgwMTB9.avJWxnQ-Nom5ICE4tMB1vEWptsDf_MP5SI1qjzP_xuM';
+  private apiUrl = 'https://app-e988bfc5-a6ee-41bb-a6af-e418a4b27735.cleverapps.io/api/stories';
+  private token: string | null = localStorage.getItem('token');
+  private castToken: string | number | (string | number)[] = this.token as string | number | (string | number)[];
 
   constructor(private http: HttpClient) { }
 
   getHistories(): Observable<any> {
-    const valor: string | null = localStorage.getItem('token')
-    const valorCasteado: string | number | (string | number)[] = valor as string | number | (string | number)[];
     const headers = new HttpHeaders({
       'User-Agent': 'Insomnia/2023.5.5',
-      'x-access-token':valorCasteado
+      'x-access-token':this.castToken
     });
 
-    return this.http.get(this.apiUrl, { headers });
+    return this.http.get(`${this.apiUrl}/getStories`, { headers });
+  }
+
+  getStoryById(id: number): Observable<any> {
+    const url = `${this.apiUrl}/getStoryById/${id}`;
+    const headers = new HttpHeaders({
+      'User-Agent': 'Insomnia/2023.5.5',
+      'x-access-token':this.castToken
+    });
+    return this.http.get(url, { headers });
   }
 }
